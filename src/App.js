af18f07fc2f552b1c65578534eff8231b03c62c5
Osak Dawid <dawidosak32@gmail.com>
2023-05-06 13:03:27 +0200

    dodano

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from './components/QuizComponent';
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+  
+    <QuizComponent/>
+  
+  );
+}
+
+export default App;
